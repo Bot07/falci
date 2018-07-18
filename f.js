2d78d01e02fedff2bb141eda248cb71a9aaa2dda
domates06 <35399633+Bot07@users.noreply.github.com>
2018-07-19 00:05:07 +0300

    Update f.js

--- f.js
+++ f.js
@@ -6,7 +6,23 @@ const client = new Discord.Client();
 
 var prefix = 'f!' 
 
+client.on("ready", () => {client.user.setGame('o!yardım | Kivi mi? Portakal mı? | Sunucu Sayısı: ' + client.guilds.size , 'https://twitch.tv/GardropFuat') 
 
+
+
+}); 
+
+
+
+client.on('ready', () => { 
+
+
+
+   console.log(`Bot${client.user.tag} adı ile giriş yaptı!`); 
+
+
+
+ }); 
 
 
 
