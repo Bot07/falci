570a939e3b9c2e5a14fa002492a401886d870a2f
domates06 <35399633+Bot07@users.noreply.github.com>
2018-07-18 23:53:22 +0300

    Update and rename r.js to f.js

---
+++ f.js
@@ -0,0 +1,18 @@
+const embed = require('discord.js'); 
+
+const Discord = require('discord.js'); 
+
+const client = new Discord.Client();
+
+var prefix = 'f!' 
+
+client.on("ready", () => {client.user.setGame('f!yardım | Fal baktırmak ister misin? | Sunucu Sayısı: ' + client.guilds.size , https://twitch.tv/GardropFuat') 
+
+}); 
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN)
