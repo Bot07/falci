9b334b6cca09207648843eccbdef7f69899a539a
domates06 <35399633+Bot07@users.noreply.github.com>
2018-07-24 14:06:19 +0300

    Update f.js

--- f.js
+++ f.js
@@ -62,4 +62,37 @@ msg.channel.send({embed: embed})
 
 });
 
+client.on('message', msg => { 
+
+
+
+if(msg.content === prefix + 'öyardım') { 
+
+
+
+let embed = new Discord.RichEmbed()  
+
+
+
+.setColor(Math.floor(Math.random() * (0xFFFFFF + 5)))  
+
+
+
+.addField("Özel mesaj olarak gönderdim", 
+
+
+
+"Özel mesajlarını kontrol et! :postbox:") 
+
+
+
+msg.channel.send({embed: embed})  
+
+
+
+} 
+
+
+
+});
 client.login(process.env.BOT_TOKEN)
