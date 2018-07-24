8bfd7faf3da7108dd5adc13541c163c909253e86
domates06 <35399633+Bot07@users.noreply.github.com>
2018-07-24 14:21:06 +0300

    Update f.js

--- f.js
+++ f.js
@@ -95,4 +95,65 @@ msg.channel.send({embed: embed})
 
 
 });
+client.on('message', msg => { 
+
+
+
+if (msg.content === prefix + 'öyardım') { 
+
+
+
+let embed = new Discord.RichEmbed() 
+
+
+
+.setColor(Math.floor(Math.random() * (0xFFFFFF + 5))) 
+
+
+
+.addField("KOMUTLAR", 
+
+
+
+"Tüm komutlar burada ->") 
+
+
+
+.addField("Ana Komutlar", 
+
+
+
+"f!fal") 
+
+
+
+.addField("Mod Komutları",  
+
+
+
+"f!fal") 
+
+
+
+.addField("Eğlence Komutları", 
+
+
+
+"f!fal")
+
+
+
+.setFooter("Bot yapımcısı: Emir#5218'dir.") 
+
+
+
+msg.member.send({embed: embed}) 
+
+
+
+}
+
+
+
+});
 client.login(process.env.BOT_TOKEN)
