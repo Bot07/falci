c7ea71815fea8be90eef2b0ca1d9e0eb9a3bf6d3
domates06 <35399633+Bot07@users.noreply.github.com>
2018-07-25 21:50:52 +0300

    Update f.js

--- f.js
+++ f.js
@@ -1284,11 +1284,9 @@ let embed = new Discord.RichEmbed()
 
 
 
-'Falına bakabilmem için sunucudaki "fal" kanalına f!rüyafalım ve rüyanızda gördüğünüz ana tema veya nesneyi yazın. \nÖrneğin; f!rüyafalım tavşan (Kişi rüyasında tavşan görmüş)')
+'Falına bakabilmem için sunucudaki "fal" kanalına f!rüyafalım ve rüyyanızda gördüğünüz ana tema veya nesneyi yazın. \nÖrneğin; f!rüyafalım tavşan (Kişi rüyasında tavşan görmüş)')
 
-.addField('Demo sürüm olduğu için aradığınız konuda rüya falı bulamayabilirsiniz.';
 
-	  'Bu durumda f!internetfalım ve rüyanızda gördüğünüz ana tema veya nesneyi yazarak ulaşabilirsiniz!')
 
 msg.channel.send({embed: embed})
 
