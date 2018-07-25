ac50aaa89a62e60a2a9b0831f272edd3880a4a46
domates06 <35399633+Bot07@users.noreply.github.com>
2018-07-25 20:05:02 +0300

    Update f.js

--- f.js
+++ f.js
@@ -899,5 +899,14 @@ const channel = msg.guild.channels.find('name', "fal")
 channel.send('Rüyada üç görmek, rüyanın genel mahiyetine uygun şekilde değerlendirilmelidir. Görülen diğer nesne ve olaylar rüyanın asıl anlamını oluşturacağından en doğru yorum bu şekilde olacaktır. Üç görmek kişiye zamanla ilgili bilgi verir ve olması istenen bir dilek için kişinin üç vakit beklemesi gerektiğinin ve sonunda hayalinin gerçek olacağının müjdesini de verir.')
 		}
 	});
-
+client.on('message', msg => {
+	
+	if(msg.author.bot) return;
+		
+		if (msg.content.toLowerCase().match(/(f!rüyafalım  )/g)) {
+			if(!msg.guild) return;
+const channel = msg.guild.channels.find('name', "fal")
+channel.send('Falcı olabilirim fakat müneccim değilim! Rüyanda ne gördüğünü yazmalısın!')
+		}
+});
 client.login(process.env.BOT_TOKEN)
